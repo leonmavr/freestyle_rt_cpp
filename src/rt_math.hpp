4fe6d85ec0e21074754a873dc6baf4f1216883a4
Leo Mav <0xleo.git@gmail.com>
2025-09-30 18:49:12 +0200

    1D instead of 2D vector for Mat, use .at method to address it

--- src/rt_math.hpp
+++ src/rt_math.hpp
@@ -1,125 +1,128 @@
-#ifndef RT_MATH_H 
+#ifndef RT_MATH_H
 #define RT_MATH_H
 
 #include <cmath>
 #include <cstdint>
 #include <iostream>
 
-template <typename T> struct Xyz;
+template <typename T>
+struct Xyz;
 // aliases for different types of data - use those for operations
-using Vec3u8  = Xyz<uint8_t>;
+using Vec3u8 = Xyz<uint8_t>;
 using Vec3i32 = Xyz<int32_t>;
-using Vec3f   = Xyz<float>;
+using Vec3f = Xyz<float>;
 
-template <typename T> struct Xyz {
+template <typename T>
+struct Xyz {
   T x, y, z;
   Xyz() : x(0), y(0), z(0) {}
   Xyz(T x, T y, T z) : x(x), y(y), z(z) {}
   explicit Xyz(T val) : x(val), y(val), z(val) {}
+//------------------------------------------------------------------------------
+// vector to vector overloaded operators
+//------------------------------------------------------------------------------
+#define VECTOR_OPS(OP)                                       \
+  Xyz<T> operator OP(const Xyz<T>& other) const {            \
+    return Xyz<T>(x OP other.x, y OP other.y, z OP other.z); \
+  }                                                          \
+  Xyz<T>& operator OP(const Xyz<T>& other) {                 \
+    x OP## = other.x;                                        \
+    y OP## = other.y;                                        \
+    z OP## = other.z;                                        \
+    return *this;                                            \
+  }
+  VECTOR_OPS(+)
+  VECTOR_OPS(-)
+  VECTOR_OPS(*)
+  VECTOR_OPS(/)
+#undef VECTOR_OPS
+//------------------------------------------------------------------------------
+// vector to scalar overloaded operators
+//------------------------------------------------------------------------------
+#define SCALAR_OPS(OP)                                                        \
+  template <typename U>                                                       \
+  auto operator OP(const U scalar) const->Xyz<decltype(T {} OP U{})> {        \
+    return Xyz<decltype(T {} OP U{})>(x OP scalar, y OP scalar, z OP scalar); \
+  }                                                                           \
+  template <typename U>                                                       \
+  Xyz<T>& operator OP(const U scalar) {                                       \
+    x OP## = scalar;                                                          \
+    y OP## = scalar;                                                          \
+    z OP## = scalar;                                                          \
+    return *this;                                                             \
+  }
+  SCALAR_OPS(+)
+  SCALAR_OPS(-)
+  SCALAR_OPS(*)
+  SCALAR_OPS(/)
+#undef SCALAR_OPS
   //------------------------------------------------------------------------------
-  // vector to vector overloaded operators
-  //------------------------------------------------------------------------------
-  #define VECTOR_OPS(OP)                                                         \
-    Xyz<T> operator OP(const Xyz<T> &other) const {                              \
-      return Xyz<T>(x OP other.x, y OP other.y, z OP other.z);                   \
-    }                                                                            \
-    Xyz<T>& operator OP(const Xyz<T> &other) {                                   \
-      x OP##= other.x;                                                           \
-      y OP##= other.y;                                                           \
-      z OP##= other.z;                                                           \
-      return *this;                                                              \
-    }
-    VECTOR_OPS(+)
-    VECTOR_OPS(-)
-    VECTOR_OPS(*)
-    VECTOR_OPS(/)
-  #undef VECTOR_OPS
-  //------------------------------------------------------------------------------
-  // vector to scalar overloaded operators
-  //------------------------------------------------------------------------------
-  #define SCALAR_OPS(OP)                                                         \
-    template <typename U>                                                        \
-    auto operator OP(const U scalar) const -> Xyz<decltype(T{} OP U{})> {        \
-      return Xyz<decltype(T{} OP U{})>(x OP scalar, y OP scalar, z OP scalar); \
-    }                                                                            \
-    template <typename U>                                                        \
-    Xyz<T>& operator OP(const U scalar) {                                        \
-      x OP##= scalar;                                                            \
-      y OP##= scalar;                                                            \
-      z OP##= scalar;                                                            \
-      return *this;                                                              \
-    }
-    SCALAR_OPS(+)
-    SCALAR_OPS(-)
-    SCALAR_OPS(*)
-    SCALAR_OPS(/)
-  #undef SCALAR_OPS 
-  //------------------------------------------------------------------------------
-  // other operators 
+  // other operators
   //------------------------------------------------------------------------------
   // unary negate
   Xyz<T> operator-() const { return Xyz<T>(-x, -y, -z); }
   // when doing std::cout << xyz;
   friend std::ostream& operator<<(std::ostream& os, const Xyz& xyz) {
-    os << "[" <<  xyz.x << ", " << xyz.y << ", " << xyz.z << "]";
+    os << "[" << xyz.x << ", " << xyz.y << ", " << xyz.z << "]";
     return os;
   }
   //------------------------------------------------------------------------------
   // common vector operations
   //------------------------------------------------------------------------------
-  T dot(const Xyz<T> &other) const { return x * other.x + y * other.y + z * other.z; }
+  T dot(const Xyz<T>& other) const {
+    return x * other.x + y * other.y + z * other.z;
+  }
   auto normSq() const -> decltype(T{} * T{}) { return x * x + y * y + z * z; }
   float norm() const { return std::sqrt(static_cast<float>(normSq())); }
   Xyz<float> unit() const {
     float n = norm();
     return Xyz<float>(x / n, y / n, z / n);
   }
-  float cos(const Xyz<T> &other) const {
+  float cos(const Xyz<T>& other) const {
     const float n1 = norm();
     const float n2 = other.norm();
     return dot(other) / (n1 * n2);
   }
-  float angle(const Xyz<T> &other) const { return std::acos(cos(other)); }
-  Xyz<T> cross(const Xyz<T> &other) const {
+  float angle(const Xyz<T>& other) const { return std::acos(cos(other)); }
+  Xyz<T> cross(const Xyz<T>& other) const {
     return Xyz<T>(y * other.z - z * other.y, z * other.x - x * other.z,
                   x * other.y - y * other.x);
   }
-  void reflectAbout(const Xyz<T> &axis) {
+  void reflectAbout(const Xyz<T>& axis) {
     // idea and formula from Bisqwit: youtube.com/watch?v=N8elxpSu9pw&t=208s
     const auto n = axis.unit();
     double v = dot(n);
-    *this = Xyz<T>(
-      static_cast<T>(2*v*n.x - x),
-      static_cast<T>(2*v*n.y - y),
-      static_cast<T>(2*v*n.z - z)
-    );
+    *this =
+        Xyz<T>(static_cast<T>(2 * v * n.x - x), static_cast<T>(2 * v * n.y - y),
+               static_cast<T>(2 * v * n.z - z));
   }
 };
 
 //-------------------------------------------------------------------------------
 // non-member operations with scalar to vector
 //-------------------------------------------------------------------------------
-#define SCALAR2VEC_OPS(OP)                                                      \
-  template <typename T, typename U>                                             \
-  auto operator OP (U scalar, const Xyz<T> &vec) -> Xyz<decltype(U{} OP T{})> { \
-    return vec OP scalar; \
+#define SCALAR2VEC_OPS(OP)                                                    \
+  template <typename T, typename U>                                           \
+  auto operator OP(U scalar, const Xyz<T>& vec)->Xyz<decltype(U {} OP T{})> { \
+    return vec OP scalar;                                                     \
   }
-  template <typename T, typename U>
-  auto operator -(U scalar, const Xyz<T> &vec) -> Xyz<decltype(U{} - T{})> {
-    return Xyz<decltype(U{} - T{})> {
-       scalar - vec.x, scalar - vec.y, scalar - vec.z, 
-    };
-  }
-  SCALAR2VEC_OPS(+)
-  SCALAR2VEC_OPS(*)
+template <typename T, typename U>
+auto operator-(U scalar, const Xyz<T>& vec) -> Xyz<decltype(U{} - T{})> {
+  return Xyz<decltype(U{} - T{})>{
+      scalar - vec.x,
+      scalar - vec.y,
+      scalar - vec.z,
+  };
+}
+SCALAR2VEC_OPS(+)
+SCALAR2VEC_OPS(*)
 #undef SCALAR2VEC_OPS
 
-
 //-------------------------------------------------------------------------------
-// Matrix operations 
+// Matrix operations
 //-------------------------------------------------------------------------------
 class Matrix3x3 {
-public:
+ public:
   Matrix3x3() {
     rows[0] = Xyz<float>(1.0f, 0.0f, 0.0f);
     rows[1] = Xyz<float>(0.0f, 1.0f, 0.0f);
@@ -134,23 +137,18 @@ public:
     RotateY(angley_rad);
     RotateX(anglex_rad);
   }
-  // matrix to vector multiplication 
+  // matrix to vector multiplication
   Xyz<float> operator*(const Xyz<float>& v) const {
-    return Xyz<float>(
-      rows[0].dot(v),
-      rows[1].dot(v),
-      rows[2].dot(v)
-    );
+    return Xyz<float>(rows[0].dot(v), rows[1].dot(v), rows[2].dot(v));
   }
-  // matrix to matrix multiplication 
+  // matrix to matrix multiplication
   Matrix3x3 operator*(const Matrix3x3& other) const {
     Matrix3x3 result;
     // transpose the other matrix to take dot products later
     Xyz<float> otherCols[3] = {
-      Xyz<float>(other.rows[0].x, other.rows[1].x, other.rows[2].x),
-      Xyz<float>(other.rows[0].y, other.rows[1].y, other.rows[2].y),
-      Xyz<float>(other.rows[0].z, other.rows[1].z, other.rows[2].z)
-    };
+        Xyz<float>(other.rows[0].x, other.rows[1].x, other.rows[2].x),
+        Xyz<float>(other.rows[0].y, other.rows[1].y, other.rows[2].y),
+        Xyz<float>(other.rows[0].z, other.rows[1].z, other.rows[2].z)};
     for (int i = 0; i < 3; i++) {
       result.rows[i].x = rows[i].dot(otherCols[0]);
       result.rows[i].y = rows[i].dot(otherCols[1]);
@@ -166,7 +164,7 @@ public:
     return os;
   }
 
-private:
+ private:
   Xyz<float> rows[3];
   void RotateX(float angle) {
     const float c = cos(angle), s = sin(angle);
@@ -179,8 +177,8 @@ private:
   void RotateY(float angle) {
     const float c = cos(angle), s = sin(angle);
     Matrix3x3 rotY;
-    rotY.rows[0] = Xyz<float>(c , 0, s);
-    rotY.rows[1] = Xyz<float>(0 , 1, 0);
+    rotY.rows[0] = Xyz<float>(c, 0, s);
+    rotY.rows[1] = Xyz<float>(0, 1, 0);
     rotY.rows[2] = Xyz<float>(-s, 0, c);
     *this = rotY * (*this);
   }
@@ -188,10 +186,10 @@ private:
     const float c = cos(angle), s = sin(angle);
     Matrix3x3 rotZ;
     rotZ.rows[0] = Xyz<float>(c, -s, 0);
-    rotZ.rows[1] = Xyz<float>(s, c , 0);
-    rotZ.rows[2] = Xyz<float>(0, 0 , 1);
+    rotZ.rows[1] = Xyz<float>(s, c, 0);
+    rotZ.rows[2] = Xyz<float>(0, 0, 1);
     *this = rotZ * (*this);
   }
 };
 
-#endif // RT_MATH_H
+#endif  // RT_MATH_H
