babe1d9be218ab24842301dcaca43f270940a496
leonmavr <0xleo.git@gmail.com>
2025-10-14 01:30:35 +0200

    `Xyz` member accessing and misc.:
    - Define data in a union of a 3-array and x,y,z to be able to alias them using .x, .y, .z
    - Bugix in `ReflectAbout`

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,9 @@
-#include <iostream>
-#include "vec.hpp"
 #include "mat3x3.hpp"
+#include "vec.hpp"
+#include <iostream>
 
-int main (int argc, char *argv[]) {
-    Mat3x3 mat1, mat2;
-    mat1.rows[0] = {3,2,1};
-    auto mat_t = mat1.Transpose();
-    std::cout << mat_t << std::endl;
-    return 0;
+int main(int argc, char *argv[]) {
+  Mat3x3 mat1, mat2;
+  mat1.RotateX(3.14/6);
+  std::cout << mat1 << std::endl;
 }
--- src/math/mat3x3.hpp
+++ src/math/mat3x3.hpp
@@ -1,10 +1,10 @@
 #ifndef MATH_3x3_HPP_
 #define MATH_3x3_HPP_
 
+#include "vec.hpp"
+#include <array>
 #include <cstdint>
 #include <iostream>
-#include <array>
-#include "vec.hpp"
 
 //-----------------------------------------------------------------------
 // Matrix operations
@@ -31,26 +31,23 @@ struct Mat3x3 {
   // operators
   //---------------------------------------------------------------------
   // matrix to vector multiplication
-  Xyz<float> operator*(const Xyz<float>& v) const {
-    return Xyz<float>((*this)[0].Dot(v),
-                      (*this)[1].Dot(v),
-                      (*this)[2].Dot(v));
+  Xyz<float> operator*(const Xyz<float> &v) const {
+    return Xyz<float>((*this)[0].Dot(v), (*this)[1].Dot(v), (*this)[2].Dot(v));
   }
 
-  Mat3x3 operator*(const Mat3x3& other) const {
-  Mat3x3 result;
-  Mat3x3 other_t = other.Transpose(); // so we can access columns easily
-                        std::cout << other << std::endl;
+  Mat3x3 operator*(const Mat3x3 &other) const {
+    Mat3x3 result;
+    Mat3x3 other_t = other.Transpose(); // so we can access columns easily
 
-  for (int i = 0; i < 3; ++i) {
-    for (int j = 0; j < 3; ++j) {
-        std::cout << rows[i]  << " * "  << other_t[j] << " = " << 
-rows[i].Dot(other_t[j]) << std::endl;
-      result[i][j] = rows[i].Dot(other_t[j]);
+    for (int i = 0; i < 3; ++i) {
+      for (int j = 0; j < 3; ++j) {
+        std::cout << rows[i] << " * " << other_t[j] << " = "
+                  << rows[i].Dot(other_t[j]) << std::endl;
+        result[i][j] = rows[i].Dot(other_t[j]);
+      }
     }
+    return result;
   }
-  return result;
-}
   Mat3x3 Transpose() const {
     Mat3x3 t;
     t.rows[0] = Xyz<float>(rows[0][0], rows[1][0], rows[2][0]);
@@ -59,21 +56,23 @@ rows[i].Dot(other_t[j]) << std::endl;
     return t;
   }
 
-  friend std::ostream& operator<<(std::ostream& os, const Mat3x3& m) {
+  friend std::ostream &operator<<(std::ostream &os, const Mat3x3 &m) {
     os << "[" << m[0] << std::endl
        << " " << m[1] << std::endl
        << " " << m[2] << "]";
     return os;
   }
 
-    Xyz<float>& operator[](int i) { 
-    if (i < 0 || i >= 3) throw std::out_of_range("Matrix index out of range");
-    return rows[i]; 
+  Xyz<float> &operator[](int i) {
+    if (i < 0 || i >= 3)
+      throw std::out_of_range("Matrix index out of range");
+    return rows[i];
   }
 
-  const Xyz<float>& operator[](int i) const { 
-    if (i < 0 || i >= 3) throw std::out_of_range("Matrix index out of range");
-    return rows[i]; 
+  const Xyz<float> &operator[](int i) const {
+    if (i < 0 || i >= 3)
+      throw std::out_of_range("Matrix index out of range");
+    return rows[i];
   }
 
   void RotateX(float angle) {
@@ -87,8 +86,8 @@ rows[i].Dot(other_t[j]) << std::endl;
   void RotateY(float angle) {
     const float c = std::cos(angle), s = std::sin(angle);
     Mat3x3 rotY;
-    rotY[0] = Xyz<float>(c , 0, s);
-    rotY[1] = Xyz<float>(0 , 1, 0);
+    rotY[0] = Xyz<float>(c, 0, s);
+    rotY[1] = Xyz<float>(0, 1, 0);
     rotY[2] = Xyz<float>(-s, 0, c);
     *this = rotY * (*this);
   }
@@ -96,11 +95,10 @@ rows[i].Dot(other_t[j]) << std::endl;
     const float c = std::cos(angle), s = std::sin(angle);
     Mat3x3 rotZ;
     rotZ[0] = Xyz<float>(c, -s, 0);
-    rotZ[1] = Xyz<float>(s,  c, 0);
-    rotZ[2] = Xyz<float>(0,  0, 1);
+    rotZ[1] = Xyz<float>(s, c, 0);
+    rotZ[2] = Xyz<float>(0, 0, 1);
     *this = rotZ * (*this);
   }
 };
 
-
-#endif  // MATH_3x3_HPP_
+#endif // MATH_3x3_HPP_
