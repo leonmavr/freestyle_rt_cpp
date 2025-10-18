3e63a39e33ba52989366dd772024bd6a1833c194
leonmavr <0xleo.git@gmail.com>
2025-10-18 23:09:01 +0200

    camera: use projected plane to check visibility

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,9 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-  Mat3x3 mat1, mat2;
   Camera cam(100, 80, 60);
-  Vec3f p = {200, -100, 900};
-  Vec3f p2 = {200, -1000, 450};
-  std::cout << cam.Project(p) << std::endl;
-  std::cout << cam.IsVisible(p2) << ", " << cam.Project(p2) << std::endl;
-
-  mat1.RotateX(3.14 / 6);
-  std::cout << mat1 << std::endl;
+  for (int i = 0; i < 10; ++i) {
+    Vec3f p = {200, 200 * i, 600};
+    std::cout << cam.Project(p).first << ", " << cam.Project(p).second << std::endl;
+  }
 }
