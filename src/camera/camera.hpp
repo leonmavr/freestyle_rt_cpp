5acaf1eb2e7a47b0ab7bdb060e6a766423ca6a96
leonmavr <0xleo.git@gmail.com>
2025-10-18 23:09:01 +0200

    camera: use projected plane to check visibility

--- src/camera/camera.hpp
+++ src/camera/camera.hpp
@@ -4,6 +4,7 @@
 #include "mat3x3.hpp"
 #include "vec.hpp"
 #include <cmath> // M_PI
+#include <utility>
 
 // pinhole camera model
 class Camera {
@@ -15,35 +16,32 @@ public:
       : center_(center),
         rot_(rot),
         focal_length_(focal_length),
-        plane_height_(2 * focal_length * std::tan(Deg2Rad(fovy_deg) / 2)),
-        plane_width_(2 * focal_length * std::tan(Deg2Rad(fovx_deg) / 2)) {}
+        plane_height_(2 * focal_length *
+                      std::tan(std::abs(Deg2Rad(fovy_deg)) / 2)),
+        plane_width_(2 * focal_length *
+                     std::tan(std::abs(Deg2Rad(fovx_deg)) / 2)) {}
   unsigned width() const { return plane_width_; }
   unsigned height() const { return plane_height_; }
+
   // perspective (pinhole) transform
-  Vec3i32 Project(Vec3f point) {
+  std::pair<Vec3i32, bool> Project(Vec3f point) {
     Vec3f point_c = World2Cam(point); // in camera (3D) coords
     // to camera plane (2D)
-    return 
+    auto projected =  
         Vec3i32{static_cast<int>(focal_length_ * point_c.x / point_c.z),
                 static_cast<int>(focal_length_ * point_c.y / point_c.z),
                 0};
-  }
-  template <typename T> bool IsVisible(Xyz<T> point) {
-    Vec3f pc = World2Cam(point);
-    // if it's behind the camera it's never visible
-    if (pc.z <= 0.0f)
-      return false;
-    // normalized direction ratios
-    float tx = pc.x / pc.z;
-    float ty = pc.y / pc.z;
-    float half_fovx = std::atan(plane_width_ / (2 * focal_length_));
-    float half_fovy = std::atan(plane_height_ / (2 * focal_length_));
-    return std::abs(tx) <= std::tan(half_fovx) &&
-           std::abs(ty) <= std::tan(half_fovy);
+    bool is_visible = (point.z > 0) && // invisible behind the camera
+                      (projected.x >= 0) &&
+                      (projected.x < plane_width_) &&
+                      (projected.y >= 0) &&
+                      (projected.y < plane_height_);
+    return std::make_pair(projected, is_visible);
   }
 
-  void Translate(Vec3f vec) {} // TODO
-  void Rotate(Mat3x3 mat) {}   // TODO
+  void Translate(Vec3f dxdydz) { center_ += dxdydz; } 
+  void Rotate(Mat3x3 mat) { rot_ = rot_ * mat; }
+
 private:
   // center of projection
   Vec3f center_;
@@ -51,6 +49,7 @@ private:
   float focal_length_;
   unsigned plane_height_;
   unsigned plane_width_;
+
   template <typename T>
   // world coordinates to camera-centered and rotated coordinates
   Vec3f World2Cam(Xyz<T> point) {
