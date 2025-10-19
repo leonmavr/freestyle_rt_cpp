fa9637cc6248ad31c91ae8970c2223b73108f822
leonmavr <0xleo.git@gmail.com>
2025-10-19 18:11:33 +0200

    Add a bounding box for camera in world coords

--- src/camera/camera.hpp
+++ src/camera/camera.hpp
@@ -6,6 +6,12 @@
 #include <cmath> // M_PI
 #include <utility>
 
+
+struct AABB {
+  Vec3f min;
+  Vec3f max;
+};
+
 // pinhole camera model
 class Camera {
   float Deg2Rad(float deg) { return deg * M_PI / 180; }
@@ -39,7 +45,49 @@ public:
     return std::make_pair(projected, is_visible);
   }
 
+  // unproject a point from the camera plane to world coordinates
+  // the point at the camera plane is at z = -focal_length
+  Vec3f UnprojectPlanePoint(float plane_x, float plane_y) const {
+    Vec3f p_cam{plane_x, plane_y, focal_length_};
+    Mat3x3 R_inv = rot_.Transpose(); // inverse = transpose in this case
+    return center_ + R_inv * p_cam;
+  }
+
+  // axis-aligned world-space AABB that encloses the rectangle on the
+  // camera plane defined by top-left (tlx,tly) and bottom-right (brx,bry).
+  AABB FindWorldAABB(int top_left_x, int top_left_y,
+                     int bottom_right_x, int bottom_right_y) const {
+    Vec3f pTL = UnprojectPlanePoint(static_cast<float>(top_left_x),
+                                    static_cast<float>(top_left_y));
+    Vec3f pTR = UnprojectPlanePoint(static_cast<float>(bottom_right_x),
+                                    static_cast<float>(top_left_y));
+    Vec3f pBL = UnprojectPlanePoint(static_cast<float>(top_left_x),
+                                    static_cast<float>(bottom_right_y));
+    Vec3f pBR = UnprojectPlanePoint(static_cast<float>(bottom_right_x),
+                                    static_cast<float>(bottom_right_y));
+    Vec3f mn{
+      std::min(std::min(pTL.x, pTR.x), std::min(pBL.x, pBR.x)),
+      std::min(std::min(pTL.y, pTR.y), std::min(pBL.y, pBR.y)),
+      std::min(std::min(pTL.z, pTR.z), std::min(pBL.z, pBR.z))
+    };
+    Vec3f mx{
+      std::max(std::max(pTL.x, pTR.x), std::max(pBL.x, pBR.x)),
+      std::max(std::max(pTL.y, pTR.y), std::max(pBL.y, pBR.y)),
+      std::max(std::max(pTL.z, pTR.z), std::max(pBL.z, pBR.z))
+    };
+    return AABB{mn, mx};
+  }
+
+  AABB WorldAABB() const {
+    int half_w = plane_width_ / 2;
+    int half_h = plane_height_ / 2;
+    // top-left (x: -half_w, y: +half_h), bottom-right (x: +half_w - 1, y: -half_h)
+    return FindWorldAABB(-half_w, half_h - 1, half_w - 1, -half_h);
+  }
+
+  // TODO: re-compute world AABB
   void Translate(Vec3f dxdydz) { center_ += dxdydz; } 
+  // TODO: re-compute world AABB
   void Rotate(Mat3x3 mat) { rot_ = rot_ * mat; }
 
 private:
--- src/ppm_writer/ppm_writer.hpp
+++ src/ppm_writer/ppm_writer.hpp
@@ -1,6 +1,7 @@
 #ifndef PPM_WRITER
 #define PPM_WRITER
 
+#include "common.hpp"
 #include <fstream>
 #include <iostream>
 #include <stdexcept>
@@ -10,10 +11,10 @@
 
 namespace Ppm {
 struct Rgb {
-    uint8_t r, g, b;
+  uint8_t r, g, b;
 };
 
-void SaveMat(const std::string &filename, const Mat<Rgb> &mat) {
+void SaveMat(const std::string &filename, const Image &mat) {
   std::ofstream file(filename);
   if (!file)
     throw std::runtime_error("ERROR: Could not write to file " + filename);
@@ -23,7 +24,7 @@ void SaveMat(const std::string &filename, const Mat<Rgb> &mat) {
   file << 255 /* max intensity for uint8 */ << "\n";
   for (unsigned y = 0; y < mat.height; ++y) {
     for (unsigned x = 0; x < mat.width; ++x) {
-      const Rgb &pixel = mat.at(y, x);
+      auto pixel = mat.at(y, x);
       file << static_cast<int>(pixel.r) << " " << static_cast<int>(pixel.g)
            << " " << static_cast<int>(pixel.b) << " ";
     }
